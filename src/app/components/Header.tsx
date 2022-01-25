ce099e408b2078ff0355db3dc1dc4d48dceaf969
papata <papata0591@gmail.com>
2022-01-25 23:26:45 +0700

    tttt

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 interface HeaderProps {
-    titleHeader: string;
+    titleHeader1: string;
 }
 
 const Header =  (headerProps: HeaderProps) => {
-    const { titleHeader } = headerProps
+    const { titleHeader1 } = headerProps
     return <>
         <div className="pt-10 text-3xl font-bold">
-            { titleHeader }
+            { titleHeader1 }
         </div>
         <div className="pt-5">
             Your current timezone is: {Intl.DateTimeFormat().resolvedOptions().timeZone}
